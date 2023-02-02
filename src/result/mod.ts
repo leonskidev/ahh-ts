a9550f86765602795fbadf88cfe899e6204ff185
leonskidev <leonskidev@pm.me>
2023-02-02 23:57:28 +0000

    feat!: make result functions more suited to ts

--- src/result/mod.ts
+++ src/result/mod.ts
@@ -1,4 +1,7 @@
 import { None, Option } from "../option/mod.ts";
+// NOTE: this is needed for documentation
+// deno-lint-ignore no-unused-vars
+import type { Some } from "../option/mod.ts";
 
 /** Represents a {@linkcode Result} that was erroneous. */
 export type Err<E extends Error> = E;
@@ -13,8 +16,274 @@ export type Result<T, E extends Error> = Err<E> | Ok<T>;
 /** Functionality for {@linkcode Result}. */
 export class R {
   /**
-   * Creates an {@linkcode Ok} from the return value of `f`, otherwise an
-   * {@linkcode Err} if it throws.
+   * Returns whether {@linkcode res} is an {@linkcode Ok}.
+   *
+   * ## Examples
+   *
+   * ```ts
+   * import { R } from "./mod.ts";
+   *
+   * console.log(R.isOk(1)); // true
+   * console.log(R.isOk(Error())); // false
+   * ```
+   */
+  static isOk<T, E extends Error>(res: Result<T, E>): res is Ok<T> {
+    return !R.isErr(res);
+  }
+
+  /**
+   * Returns whether {@linkcode res} is an {@linkcode Err}.
+   *
+   * ## Examples
+   *
+   * ```ts
+   * import { R } from "./mod.ts";
+   *
+   * console.log(R.isErr(1)); // false
+   * console.log(R.isErr(Error())); // true
+   * ```
+   */
+  static isErr<T, E extends Error>(res: Result<T, E>): res is Err<E> {
+    return res instanceof Error;
+  }
+
+  /**
+   * Returns whether {@linkcode res} strictly equals {@linkcode value}, or
+   * `false` if it is an {@linkcode Err}.
+   *
+   * ## Examples
+   *
+   * ```ts
+   * import { R } from "./mod.ts";
+   *
+   * console.log(R.contains(1, 1)); // true
+   * console.log(R.contains(1, 2)); // false
+   * console.log(R.contains(Error(), 1)); // false
+   * ```
+   */
+  static contains<T, E extends Error>(res: Result<T, E>, value: T): boolean {
+    return R.isOk(res) ? res === value : false;
+  }
+
+  /**
+   * Returns whether {@linkcode res} strictly equals {@linkcode value}, or
+   * `false` if it is an {@linkcode Ok}.
+   *
+   * This only checks whether {@linkcode res} and {@linkcode value} have the
+   * same `name` and `message`.
+   *
+   * ## Examples
+   *
+   * ```ts
+   * import { R } from "./mod.ts";
+   *
+   * console.log(R.containsErr(1, Error())); // false
+   * console.log(R.containsErr(Error(), Error())); // true
+   * console.log(R.containsErr(Error(), Error("nope"))); // false
+   * ```
+   */
+  static containsErr<T, E extends Error>(res: Result<T, E>, value: E): boolean {
+    return R.isErr(res)
+      ? res.name === value.name && res.message === value.message
+      : false;
+  }
+
+  /**
+   * Calls {@linkcode f} with {@linkcode res}, and returns the original
+   * {@linkcode res}.
+   *
+   * ## Examples
+   *
+   * ```ts
+   * import { R } from "./mod.ts";
+   *
+   * console.log(R.inspect(1, (i) => i + 1)); // 1
+   * console.log(R.inspect(Error(), (i: number) => i + 1)); // Error()
+   * ```
+   */
+  static inspect<T, E extends Error>(
+    res: Result<T, E>,
+    f: (_: T) => unknown,
+  ): Result<T, E> {
+    if (R.isOk(res)) f(res);
+    return res;
+  }
+
+  /**
+   * Calls {@linkcode f} with {@linkcode res}, and returns the original
+   * {@linkcode res}.
+   *
+   * ## Examples
+   *
+   * ```ts
+   * import { R } from "./mod.ts";
+   *
+   * console.log(R.inspectErr(1, () => Error("nope"))); // 1
+   * console.log(R.inspectErr(Error(), () => Error("nope"))); // Error()
+   * ```
+   */
+  static inspectErr<T, E extends Error>(
+    res: Result<T, E>,
+    f: (_: E) => unknown,
+  ): Result<T, E> {
+    if (R.isErr(res)) f(res);
+    return res;
+  }
+
+  /**
+   * Calls {@linkcode f} with {@linkcode res}, and returns the result.
+   *
+   * ## Examples
+   *
+   * ```ts
+   * import { R } from "./mod.ts";
+   *
+   * console.log(R.map(1, (i) => i + 1)); // 2
+   * console.log(R.map(Error(), (i: number) => i + 1)); // Error()
+   * ```
+   */
+  static map<T, U, E extends Error>(
+    res: Result<T, E>,
+    f: (_: T) => U,
+  ): Result<U, E> {
+    return R.isOk(res) ? f(res) : res;
+  }
+
+  /**
+   * Calls {@linkcode f} with {@linkcode res}, and returns the result.
+   *
+   * ## Examples
+   *
+   * ```ts
+   * import { R } from "./mod.ts";
+   *
+   * console.log(R.map(1, (_) => Error("new"))); // 1
+   * console.log(R.map(Error(), (_) => Error("new"))); // Error("new")
+   * ```
+   */
+  static mapErr<T, E extends Error, F extends Error>(
+    res: Result<T, E>,
+    f: (_: E) => F,
+  ): Result<T, F> {
+    return R.isErr(res) ? f(res) : res;
+  }
+
+  /**
+   * Returns {@linkcode res} if it is an {@linkcode Ok}, or throws.
+   *
+   * ## Examples
+   *
+   * ```ts
+   * import { R } from "./mod.ts";
+   *
+   * console.log(R.unwrap(1)); // 1
+   * R.unwrap(Error()); // throws
+   * ```
+   */
+  static unwrap<T, E extends Error>(
+    res: Result<T, E>,
+  ): typeof res extends Ok<T> ? T : never {
+    if (R.isOk(res)) return res as never;
+    throw Error(`attempted to unwrap an \`Err\` value: ${res.message}`);
+  }
+
+  /**
+   * Returns {@linkcode res} if it is an {@linkcode Ok}, or returns
+   * {@linkcode default_}.
+   *
+   * ## Examples
+   *
+   * ```ts
+   * import { R } from "./mod.ts";
+   *
+   * console.log(R.unwrapOr<number, Error>(1, 2)); // 1
+   * console.log(R.unwrapOr(Error(), 2)); // 2
+   * ```
+   */
+  static unwrapOr<T, E extends Error>(res: Result<T, E>, default_: T): T {
+    return R.isOk(res) ? res : default_;
+  }
+
+  /**
+   * Return {@linkcode res} if it is an {@linkcode Ok}, or {@linkcode other}.
+   *
+   * ## Examples
+   *
+   * ```ts
+   * import { R } from "./mod.ts";
+   *
+   * console.log(R.or(1, 2)); // 1
+   * console.log(R.or<number, Error, Error>(Error(), 2)); // 2
+   * console.log(R.or(2, Error())); // 2
+   * console.log(R.or(Error("a"), Error("b"))); // Error("b")
+   * ```
+   */
+  static or<T, E extends Error, F extends Error>(
+    res: Result<T, E>,
+    other: Result<T, F>,
+  ): Result<T, F> {
+    return R.isOk(res) ? res : other;
+  }
+
+  /**
+   * Return {@linkcode other} if {@linkcode res} is {@linkcode Ok}, or
+   * {@linkcode res}.
+   *
+   * ## Examples
+   *
+   * ```ts
+   * import { R } from "./mod.ts";
+   *
+   * console.log(R.and(1, 2)); // 2
+   * console.log(R.and(Error(), 2)); // Error()
+   * console.log(R.and(2, Error())); // Error()
+   * console.log(R.and(Error("a"), Error("b"))); // Error("a")
+   * ```
+   */
+  static and<T, U, E extends Error>(
+    res: Result<T, E>,
+    other: Result<U, E>,
+  ): Result<U, E> {
+    return R.isOk(res) ? other : res;
+  }
+
+  /**
+   * Converts {@linkcode res} into a {@linkcode Some} if it is an
+   * {@linkcode Ok}.
+   *
+   * ## Examples
+   *
+   * ```ts
+   * import { R } from "./mod.ts";
+   *
+   * console.log(R.ok(1)); // 1
+   * console.log(R.ok(Error())); // undefined
+   * ```
+   */
+  static ok<T, E extends Error>(res: Result<T, E>): Option<T> {
+    return R.isOk(res) ? res : None;
+  }
+
+  /**
+   * Converts {@linkcode res} into a {@linkcode Some} if it is an
+   * {@linkcode Err}.
+   *
+   * ## Examples
+   *
+   * ```ts
+   * import { R } from "./mod.ts";
+   *
+   * console.log(R.err(1)); // undefined
+   * console.log(R.err(Error())); // Error()
+   * ```
+   */
+  static err<T, E extends Error>(res: Result<T, E>): Option<E> {
+    return R.isErr(res) ? res : None;
+  }
+
+  /**
+   * Calls {@linkcode f} and returns the result as an {@linkcode Ok}, or returns
+   * an {@linkcode Err} if it throws.
    *
    * ## Examples
    *
@@ -31,258 +300,5 @@ export class R {
     } catch (e) {
       return e;
     }
-  }
-
-  /**
-   * Returns whether a {@linkcode Result} is an {@linkcode Ok}.
-   *
-   * ## Examples
-   *
-   * ```ts
-   * import { R } from "./mod.ts";
-   *
-   * console.log(R.isOk(1)); // true
-   * console.log(R.isOk(Error())); // false
-   * ```
-   */
-  static isOk<T, E extends Error>(r: Result<T, E>): r is Ok<T> {
-    return !(r instanceof Error);
-  }
-
-  /**
-   * Returns whether a {@linkcode Result} is an {@linkcode Err}.
-   *
-   * ## Examples
-   *
-   * ```ts
-   * import { R } from "./mod.ts";
-   *
-   * console.log(R.isErr(1)); // false
-   * console.log(R.isErr(Error())); // true
-   * ```
-   */
-  static isErr<T, E extends Error>(r: Result<T, E>): r is Err<E> {
-    return r instanceof Error;
-  }
-
-  /**
-   * Returns the contained {@linkcode Ok} value.
-   *
-   * Throws if the {@linkcode Result} is an {@linkcode Err} with the provided
-   * `message`.
-   *
-   * ## Examples
-   *
-   * ```ts
-   * import { R } from "./mod.ts";
-   *
-   * console.log(R.expect(1, "returns")); // 1
-   * R.expect(Error("whoops"), "throws"); // throws
-   * ```
-   */
-  static expect<T, E extends Error>(
-    r: Result<T, E>,
-    message: string,
-  ): typeof r extends Ok<T> ? T : never {
-    if (R.isErr(r)) throw Error(message);
-    // TODO: why? also fix this
-    return r as never;
-  }
-
-  /**
-   * Returns the contained {@linkcode Err} value.
-   *
-   * Throws if the {@linkcode Result} is an {@linkcode Ok} with the provided
-   * `message`.
-   *
-   * ## Examples
-   *
-   * ```ts
-   * import { R } from "./mod.ts";
-   *
-   * console.log(R.expectErr(Error("whoops"), "returns")); // Error("whoops")
-   * R.expectErr(1, "throws"); // throws
-   * ```
-   */
-  static expectErr<T, E extends Error>(
-    r: Result<T, E>,
-    message: string,
-  ): typeof r extends Err<E> ? E : never {
-    if (R.isOk(r)) throw Error(message);
-    // TODO: why? also fix this
-    return r as never;
-  }
-
-  /**
-   * Returns the contained {@linkcode Ok} value.
-   *
-   * Throws if the {@linkcode Result} is an {@linkcode Err}.
-   *
-   * ## Examples
-   *
-   * ```ts
-   * import { R } from "./mod.ts";
-   *
-   * console.log(R.unwrap(1)); // 1
-   * R.unwrap(Error("whoops")); // throws
-   * ```
-   */
-  static unwrap<T, E extends Error>(
-    r: Result<T, E>,
-  ): typeof r extends Ok<T> ? T : never {
-    return R.expect(
-      r,
-      `called \`unwrap()\` on an \`Err\` value: ${(r as Err<E>).message}`,
-    );
-  }
-
-  /**
-   * Returns the contained {@linkcode Err} value.
-   *
-   * Throws if the {@linkcode Result} is an {@linkcode Ok}.
-   *
-   * ## Examples
-   *
-   * ```ts
-   * import { R } from "./mod.ts";
-   *
-   * console.log(R.unwrapErr(Error("whoops"))); // Error("whoops")
-   * R.unwrapErr(1); // throws
-   * ```
-   */
-  static unwrapErr<T, E extends Error>(
-    r: Result<T, E>,
-  ): typeof r extends Err<E> ? E : never {
-    return R.expectErr(
-      r,
-      `called \`unwrapErr()\` on an \`Ok\` value: ${(r as Ok<T>)}`,
-    );
-  }
-
-  /**
-   * Returns the contained {@linkcode Ok} value or the provided `default`.
-   *
-   * ## Examples
-   *
-   * ```ts
-   * import { R } from "./mod.ts";
-   *
-   * console.log(R.unwrapOr<number, Error>(1, 5)); // 1
-   * console.log(R.unwrapOr(Error("whoops"), 5)); // 5
-   * ```
-   */
-  static unwrapOr<T, E extends Error>(r: Result<T, E>, v: T): T {
-    return R.isOk(r) ? r : v;
-  }
-
-  /**
-   * Maps the contained {@linkcode Ok} value with `f`, otherwise
-   * {@linkcode Err}.
-   *
-   * ## Examples
-   *
-   * ```ts
-   * import { R } from "./mod.ts";
-   *
-   * console.log(R.map(1, (i) => i + 1)); // 2
-   * console.log(R.map<number, Error, number>(Error("whoops"), (i) => i + 1)); // Error("whoops")
-   * ```
-   */
-  static map<T, E extends Error, U>(
-    r: Result<T, E>,
-    f: (_: T) => U,
-  ): Result<U, E> {
-    return R.isOk(r) ? f(r) : r;
-  }
-
-  /**
-   * Maps the contained {@linkcode Err} value with `f`, otherwise
-   * {@linkcode Ok}.
-   *
-   * ## Examples
-   *
-   * ```ts
-   * import { R } from "./mod.ts";
-   *
-   * console.log(R.mapErr(1, (i) => Error(i.message + "bar"))); // 1
-   * console.log(R.mapErr(Error("foo"), (i) => Error(i.message + "bar"))); // Error("foobar")
-   * ```
-   */
-  static mapErr<T, E extends Error, F extends Error>(
-    r: Result<T, E>,
-    f: (_: E) => F,
-  ): Result<T, F> {
-    return R.isErr(r) ? f(r) : r;
-  }
-
-  /**
-   * Returns whether the contained {@linkcode Ok} value strictly equals `v`.
-   *
-   * ## Examples
-   *
-   * ```ts
-   * import { R } from "./mod.ts";
-   *
-   * console.log(R.contains(1, 1)); // true
-   * console.log(R.contains(1, 2)); // false
-   * console.log(R.contains(Error("whoops"), 1)); // false
-   * ```
-   */
-  static contains<T, E extends Error>(r: Result<T, E>, v: T): boolean {
-    return R.isOk(r) ? r === v : false;
-  }
-
-  /**
-   * Returns whether the contained {@linkcode Err} value strictly equals `v`.
-   *
-   * This checks all fields except for `stack` which is usually the desired
-   * behaviour.
-   *
-   * ## Examples
-   *
-   * ```ts
-   * import { R } from "./mod.ts";
-   *
-   * console.log(R.containsErr(Error("whoops"), Error("whoops"))); // true
-   * console.log(R.containsErr(Error("whoops"), Error("spoohw"))); // false
-   * console.log(R.containsErr(1, Error("whoops"))); // false
-   * ```
-   */
-  static containsErr<T, E extends Error>(r: Result<T, E>, v: E): boolean {
-    return R.isErr(r)
-      ? r.name === v.name && r.cause === v.cause && r.message === v.message
-      : false;
-  }
-
-  /**
-   * Converts an {@linkcode Ok} into an {@linkcode Option}.
-   *
-   * ## Examples
-   *
-   * ```ts
-   * import { R } from "./mod.ts";
-   *
-   * console.log(R.ok(1)); // 1
-   * console.log(R.ok(Error("whoops"))); // undefined
-   * ```
-   */
-  static ok<T, E extends Error>(r: Result<T, E>): Option<T> {
-    return R.isOk(r) ? r : None;
-  }
-
-  /**
-   * Converts an {@linkcode Err} into an {@linkcode Option}.
-   *
-   * ## Examples
-   *
-   * ```ts
-   * import { R } from "./mod.ts";
-   *
-   * console.log(R.err(Error("whoops"))); // Error("whoops")
-   * console.log(R.err(1)); // undefined
-   * ```
-   */
-  static err<T, E extends Error>(r: Result<T, E>): Option<E> {
-    return R.isErr(r) ? r : None;
   }
 }
--- src/result/test.ts
+++ src/result/test.ts
@@ -1,61 +1,73 @@
-import {
-  assert,
-  assertEquals,
-  assertStrictEquals,
-  assertThrows,
-} from "../../test_deps.ts";
+import { assert, assertStrictEquals, assertThrows } from "../../test_deps.ts";
 import { None } from "../option/mod.ts";
 import { R } from "./mod.ts";
 
-Deno.test("fn", async (t) => {
-  await t.step("ok", () => assertStrictEquals(R.fn(() => 1), 1));
-  await t.step("ok", () =>
-    assertEquals(
-      {
-        ...R.fn(() => {
-          throw Error("whoops");
-        }),
-        stack: undefined,
-      },
-      { ...Error("whoops"), stack: undefined },
-    ));
-});
+const ERROR = Error("whoops");
 
 Deno.test("is_ok", async (t) => {
   await t.step("ok", () => assert(R.isOk(1)));
-  await t.step("err", () => assert(!R.isOk(Error("whoops"))));
+  await t.step("err", () => assert(!R.isOk(ERROR)));
 });
 
 Deno.test("is_err", async (t) => {
   await t.step("ok", () => assert(!R.isErr(1)));
-  await t.step("err", () => assert(R.isErr(Error("whoops"))));
+  await t.step("err", () => assert(R.isErr(ERROR)));
 });
 
-Deno.test("expect", async (t) => {
-  await t.step("ok", () => assertStrictEquals(R.expect(1, "returns"), 1));
+Deno.test("contains", async (t) => {
+  await t.step("ok_equals", () => assert(R.contains(1, 1)));
+  await t.step("ok_not_equals", () => assert(!R.contains(1, 2)));
+  await t.step("err", () => assert(!R.contains(ERROR, 1)));
+});
+
+Deno.test("contains_err", async (t) => {
+  await t.step("ok", () => assert(!R.containsErr(1, ERROR)));
+  await t.step("err_equals", () => assert(R.containsErr(ERROR, ERROR)));
   await t.step(
-    "err",
-    () =>
-      void assertThrows(
-        () => R.expect(Error("whoops"), "throws"),
-        Error,
-        "throws",
-      ),
+    "err_not_equals",
+    () => assert(!R.containsErr(ERROR, Error("nope"))),
   );
 });
 
-Deno.test("expect_err", async (t) => {
+Deno.test("inspect", async (t) => {
   await t.step(
     "ok",
-    () => void assertThrows(() => R.expectErr(1, "throws"), Error, "throws"),
+    () => assertStrictEquals(R.inspect(1, (i) => i + 1), 1),
   );
   await t.step(
     "err",
-    () =>
-      assertEquals(
-        { ...R.expectErr(Error("whoops"), "returns"), stack: undefined },
-        { ...Error("whoops"), stack: undefined },
-      ),
+    () => assertStrictEquals(R.inspect(ERROR, (i: number) => i + 1), ERROR),
+  );
+});
+
+Deno.test("inspect_err", async (t) => {
+  await t.step(
+    "ok",
+    () => assertStrictEquals(R.inspectErr(1, (_) => Error("nope")), 1),
+  );
+  await t.step(
+    "err",
+    () => assertStrictEquals(R.inspectErr(ERROR, (_) => Error("nope")), ERROR),
+  );
+});
+
+Deno.test("map", async (t) => {
+  await t.step("ok", () => assertStrictEquals(R.map(1, (i) => i + 1), 2));
+  await t.step(
+    "err",
+    () => assertStrictEquals(R.map(ERROR, (i: number) => i + 1), ERROR),
+  );
+});
+
+Deno.test("map_err", async (t) => {
+  const ERROR_NEW = Error("new");
+  await t.step(
+    "ok",
+    () => assertStrictEquals(R.mapErr(1, (_) => ERROR_NEW), 1),
+  );
+  await t.step(
+    "err",
+    () => assertStrictEquals(R.mapErr(ERROR, (_) => ERROR_NEW), ERROR_NEW),
   );
 });
 
@@ -65,106 +77,206 @@ Deno.test("unwrap", async (t) => {
     "err",
     () =>
       void assertThrows(
-        () => R.unwrap(Error("whoops")),
+        () => R.unwrap(ERROR),
         Error,
-        "called `unwrap()` on an `Err` value: whoops",
+        `attempted to unwrap an \`Err\` value: ${ERROR.message}`,
       ),
-  );
-});
-
-Deno.test("unwrap_err", async (t) => {
-  await t.step(
-    "ok",
-    () =>
-      void assertThrows(
-        () => R.unwrapErr(1),
-        Error,
-        "called `unwrapErr()` on an `Ok` value: 1",
-      ),
-  );
-  await t.step(
-    "err",
-    () =>
-      assertEquals({ ...R.unwrapErr(Error("whoops")), stack: undefined }, {
-        ...Error("whoops"),
-        stack: undefined,
-      }),
   );
 });
 
 Deno.test("unwrap_or", async (t) => {
   await t.step(
     "ok",
-    () => assertStrictEquals(R.unwrapOr<number, Error>(1, 5), 1),
+    () => assertStrictEquals(R.unwrapOr<number, Error>(1, 2), 1),
   );
-  await t.step(
-    "err",
-    () => assertStrictEquals(R.unwrapOr(Error("whoops"), 5), 5),
-  );
+  await t.step("err", () => assertStrictEquals(R.unwrapOr(ERROR, 2), 2));
 });
 
-Deno.test("map", async (t) => {
-  await t.step("ok", () => assertEquals(R.map(1, (i) => i + 1), 2));
+Deno.test("or", async (t) => {
+  await t.step("ok_ok", () => assertStrictEquals(R.or(1, 2), 1));
+  await t.step("ok_err", () => assertStrictEquals(R.or(1, ERROR), 1));
   await t.step(
-    "err",
-    () =>
-      assertEquals(
-        R.map<number, Error, number>(Error("foo"), (i) => i + 1),
-        Error("foo"),
-      ),
+    "err_ok",
+    () => assertStrictEquals(R.or<number, Error, Error>(ERROR, 2), 2),
   );
+  await t.step("err_err", () => assertStrictEquals(R.or(ERROR, ERROR), ERROR));
 });
 
-Deno.test("map_err", async (t) => {
+Deno.test("or", async (t) => {
+  await t.step("ok_ok", () => assertStrictEquals(R.and(1, 2), 2));
+  await t.step("ok_err", () => assertStrictEquals(R.and(1, ERROR), ERROR));
   await t.step(
-    "ok",
-    () =>
-      assertEquals(
-        R.mapErr(1, (i) => Error(i.message + "bar")),
-        1,
-      ),
+    "err_ok",
+    () => assertStrictEquals(R.and(ERROR, 2), ERROR),
   );
-  await t.step(
-    "err",
-    () =>
-      assertEquals(
-        R.mapErr(Error("foo"), (i) => Error(i.message + "bar")),
-        Error("foobar"),
-      ),
-  );
-});
-
-Deno.test("contains", async (t) => {
-  await t.step("ok_equals", () => assert(R.contains(1, 1)));
-  await t.step("ok_not_equals", () => assert(!R.contains(1, 2)));
-  await t.step("err", () => assert(!R.contains(Error("whoops"), 1)));
-});
-
-Deno.test("contains_err", async (t) => {
-  await t.step("ok", () => assert(!R.containsErr(1, Error("whoops"))));
-  await t.step(
-    "err_equals",
-    () => assert(R.containsErr(Error("whoops"), Error("whoops"))),
-  );
-  await t.step(
-    "err_not_equals",
-    () => assert(!R.containsErr(Error("whoops"), Error("spoohw"))),
-  );
+  await t.step("err_err", () => assertStrictEquals(R.and(ERROR, ERROR), ERROR));
 });
 
 Deno.test("ok", async (t) => {
   await t.step("ok", () => assertStrictEquals(R.ok(1), 1));
-  await t.step("err", () => assertStrictEquals(R.ok(Error("whoops")), None));
+  await t.step("err", () => assertStrictEquals(R.ok(ERROR), None));
 });
 
 Deno.test("err", async (t) => {
   await t.step("ok", () => assertStrictEquals(R.err(1), None));
-  await t.step(
-    "err",
-    () =>
-      assertEquals({ ...R.err(Error("whoops")), stack: undefined }, {
-        ...Error("whoops"),
-        stack: undefined,
-      }),
-  );
+  await t.step("err", () => assertStrictEquals(R.err(ERROR), ERROR));
 });
+
+Deno.test("fn", async (t) => {
+  await t.step("ok", () => assertStrictEquals(R.fn(() => 1), 1));
+  await t.step("err", () =>
+    assertStrictEquals(
+      R.fn(() => {
+        throw ERROR;
+      }),
+      ERROR,
+    ));
+});
+
+// Deno.test("is_ok", async (t) => {
+//   await t.step("ok", () => assert(R.isOk(1)));
+//   await t.step("err", () => assert(!R.isOk(Error("whoops"))));
+// });
+
+// Deno.test("is_err", async (t) => {
+//   await t.step("ok", () => assert(!R.isErr(1)));
+//   await t.step("err", () => assert(R.isErr(Error("whoops"))));
+// });
+
+// Deno.test("expect", async (t) => {
+//   await t.step("ok", () => assertStrictEquals(R.expect(1, "returns"), 1));
+//   await t.step(
+//     "err",
+//     () =>
+//       void assertThrows(
+//         () => R.expect(Error("whoops"), "throws"),
+//         Error,
+//         "throws",
+//       ),
+//   );
+// });
+
+// Deno.test("expect_err", async (t) => {
+//   await t.step(
+//     "ok",
+//     () => void assertThrows(() => R.expectErr(1, "throws"), Error, "throws"),
+//   );
+//   await t.step(
+//     "err",
+//     () =>
+//       assertEquals(
+//         { ...R.expectErr(Error("whoops"), "returns"), stack: undefined },
+//         { ...Error("whoops"), stack: undefined },
+//       ),
+//   );
+// });
+
+// Deno.test("unwrap", async (t) => {
+//   await t.step("ok", () => assertStrictEquals(R.unwrap(1), 1));
+//   await t.step(
+//     "err",
+//     () =>
+//       void assertThrows(
+//         () => R.unwrap(Error("whoops")),
+//         Error,
+//         "called `unwrap()` on an `Err` value: whoops",
+//       ),
+//   );
+// });
+
+// Deno.test("unwrap_err", async (t) => {
+//   await t.step(
+//     "ok",
+//     () =>
+//       void assertThrows(
+//         () => R.unwrapErr(1),
+//         Error,
+//         "called `unwrapErr()` on an `Ok` value: 1",
+//       ),
+//   );
+//   await t.step(
+//     "err",
+//     () =>
+//       assertEquals({ ...R.unwrapErr(Error("whoops")), stack: undefined }, {
+//         ...Error("whoops"),
+//         stack: undefined,
+//       }),
+//   );
+// });
+
+// Deno.test("unwrap_or", async (t) => {
+//   await t.step(
+//     "ok",
+//     () => assertStrictEquals(R.unwrapOr<number, Error>(1, 5), 1),
+//   );
+//   await t.step(
+//     "err",
+//     () => assertStrictEquals(R.unwrapOr(Error("whoops"), 5), 5),
+//   );
+// });
+
+// Deno.test("map", async (t) => {
+//   await t.step("ok", () => assertEquals(R.map(1, (i) => i + 1), 2));
+//   await t.step(
+//     "err",
+//     () =>
+//       assertEquals(
+//         R.map<number, Error, number>(Error("foo"), (i) => i + 1),
+//         Error("foo"),
+//       ),
+//   );
+// });
+
+// Deno.test("map_err", async (t) => {
+//   await t.step(
+//     "ok",
+//     () =>
+//       assertEquals(
+//         R.mapErr(1, (i) => Error(i.message + "bar")),
+//         1,
+//       ),
+//   );
+//   await t.step(
+//     "err",
+//     () =>
+//       assertEquals(
+//         R.mapErr(Error("foo"), (i) => Error(i.message + "bar")),
+//         Error("foobar"),
+//       ),
+//   );
+// });
+
+// Deno.test("contains", async (t) => {
+//   await t.step("ok_equals", () => assert(R.contains(1, 1)));
+//   await t.step("ok_not_equals", () => assert(!R.contains(1, 2)));
+//   await t.step("err", () => assert(!R.contains(Error("whoops"), 1)));
+// });
+
+// Deno.test("contains_err", async (t) => {
+//   await t.step("ok", () => assert(!R.containsErr(1, Error("whoops"))));
+//   await t.step(
+//     "err_equals",
+//     () => assert(R.containsErr(Error("whoops"), Error("whoops"))),
+//   );
+//   await t.step(
+//     "err_not_equals",
+//     () => assert(!R.containsErr(Error("whoops"), Error("spoohw"))),
+//   );
+// });
+
+// Deno.test("ok", async (t) => {
+//   await t.step("ok", () => assertStrictEquals(R.ok(1), 1));
+//   await t.step("err", () => assertStrictEquals(R.ok(Error("whoops")), None));
+// });
+
+// Deno.test("err", async (t) => {
+//   await t.step("ok", () => assertStrictEquals(R.err(1), None));
+//   await t.step(
+//     "err",
+//     () =>
+//       assertEquals({ ...R.err(Error("whoops")), stack: undefined }, {
+//         ...Error("whoops"),
+//         stack: undefined,
+//       }),
+//   );
+// });

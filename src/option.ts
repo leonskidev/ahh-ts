48b4ae8253e19e958b5c57675dd8b349697f7988
Leon Davis <leonskidev@pm.me>
2023-07-10 17:15:45 +0100

    feat!: option
    this improves the documenttion, code, adds a few functions; and removes a
    function. the tests are also improved and check that what is returned from
    functions remains consistent

--- src/option.ts
+++ src/option.ts
@@ -1,114 +1,148 @@
 /**
- * Contains an idiomatic {@linkcode Option} type and related functions.
+ * An optional value.
  *
- * The types and functions provided in this module likely follow how you already
- * handle optional values yourself. A quick look at what the {@linkcode Option}
- * type boils down to shows how simple it is:
+ * The {@linkcode Option} type defined here is nothing special, it's the same as
+ * what the rest of the ecosystem already uses. Thanks to this, we still benefit
+ * from the built-in language features for [nullish] and [falsy] types, such as:
  *
- * ```ts
- * type Option<T> = (undefined | null) | T;
- * ```
+ * - [nullish coalescing] (`??` and `??=`); and
+ * - [optional chaining] (`?.`).
  *
- * Almost all built-in and external modules handle the ***none*** side as
- * `undefined` or `null`, and the ***some*** side as the value itself. This is
- * exactly the same as what the type above defines.
- *
- * Since optionals are not special, we still have access to the features built
- * into the language, such as:
- *
- * - [`??` and `??=`]
- * - [`?.`]
- *
- * This module simply provides extra functionality on-top of these.
- *
- * [`??` and `??=`]: https://developer.mozilla.org/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing
- * [`?.`]: https://developer.mozilla.org/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+ * [nullish]: https://developer.mozilla.org/docs/Glossary/Nullish
+ * [falsy]: https://developer.mozilla.org/docs/Glossary/Falsy
+ * [nullish coalescing]: https://developer.mozilla.org/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing
+ * [optional chaining]: https://developer.mozilla.org/docs/Web/JavaScript/Reference/Operators/Optional_chaining
  *
  * @module
  */
 
-/** A value that is either `undefined` or `null`. */
+/**
+ * A value that is [nullish]; either `undefined` or `null`.
+ *
+ * [nullish]: https://developer.mozilla.org/docs/Glossary/Nullish
+ */
 export type None = undefined | null;
-/** A value this is neither `undefined` nor `null`. */
-export type Some<T> = T;
-/** A value that is either {@linkcode Some} or {@linkcode None}. */
+/**
+ * A value that is non-[nullish].
+ *
+ * [nullish]: https://developer.mozilla.org/docs/Glossary/Nullish
+ */
+export type Some<T> = NonNullable<T>;
+/** A value that is either {@linkcode None} or {@linkcode Some}. */
 export type Option<T> = None | Some<T>;
 
 /**
- * Returns whether {@linkcode option} is a {@linkcode Some}.
+ * Returns whether `option` is a {@linkcode Some} value.
  *
  * @example
  * ```ts
- * import { default as O, Option } from "./option.ts";
+ * import { assert } from "../test_deps.ts";
+ * import O from "./option.ts";
  *
- * const num: Option<number> = 8;
- *
- * if (O.isSome(num)) {
- *   console.log(num * 2);
- * }
+ * assert(O.isSome(0));
+ * assert(O.isSome(2));
  * ```
  */
 export function isSome<T>(option: Option<T>): option is Some<T> {
-  return typeof (option) !== "undefined" && option !== null;
+  return !isNone(option);
 }
 
 /**
- * Returns whether {@linkcode option} is a {@linkcode None}.
+ * Returns whether `option` is a {@linkcode None} value.
  *
  * @example
  * ```ts
- * import { default as O, Option } from "./option.ts";
+ * import { assert } from "../test_deps.ts";
+ * import O from "./option.ts";
  *
- * const num: Option<number> = undefined;
- *
- * if (O.isNone(num)) {
- *   console.log("the number is a lie");
- * }
+ * assert(O.isNone(undefined));
+ * assert(O.isNone(null));
  * ```
  */
 export function isNone<T>(option: Option<T>): option is None {
-  return !isSome(option);
+  return typeof (option) === "undefined" || option === null;
 }
 
 /**
- * Returns {@linkcode option} with its value mapped via {@linkcode fn}, if it
- * is a {@linkcode Some}.
+ * Returns the value of `option` if it is a {@linkcode Some}, or returns `or`.
  *
  * @example
  * ```ts
- * import { default as O, Option } from "./option.ts";
+ * import { assert } from "../test_deps.ts";
+ * import O from "./option.ts";
  *
- * const num: Option<number> = 8;
- * const double = O.map(num, (i) => i * 2);
- *
- * if (O.isSome(double)) {
- *   console.log(num, "doubled is", double);
- * }
+ * assert(O.either<number>(2, 4) === 4);
+ * assert(O.either(undefined, 2) === 2);
  * ```
  */
-export function map<T, U>(
+export function either<T>(option: Option<T>, or: T): T {
+  return isSome(option) ? option : or;
+}
+
+/**
+ * Returns the result of `fn` appied to the value of `option`, if it is a
+ * {@linkcode Some}.
+ *
+ * @example
+ * ```ts
+ * import { assert } from "../test_deps.ts";
+ * import O from "./option.ts";
+ *
+ * const toString = (i: number): string => i.toString();
+ *
+ * assert(O.map(2, toString) === "2");
+ * assert(O.map(undefined, toString) === undefined);
+ * assert(O.map(null, toString) === null);
+ * ```
+ */
+export function map<T, U extends Some<unknown>>(
   option: Option<T>,
-  fn: (_: T) => Option<U> | U,
+  fn: (value: T) => U | Option<U>,
 ): Option<U> {
   return isSome(option) ? fn(option) : option;
 }
 
 /**
- * Returns {@linkcode option} if {@linkcode fn} returns `true` and it is a
- * {@linkcode Some}.
+ * Returns the values of `option` and `other` as a tuple if both are
+ * {@linkcode Some}s.
  *
  * @example
  * ```ts
- * import { default as O, Option } from "./option.ts";
+ * import { assert } from "../test_deps.ts";
+ * import O from "./option.ts";
  *
- * const num: Option<number> = 8;
- * const even = O.filter(num, (i) => i % 2 === 0);
+ * const zipped = O.zip(2, "hello");
  *
- * console.log(even ?? "num is odd");
+ * assert(O.isSome(zipped));
+ * assert(zipped[0] === 2);
+ * assert(zipped[1] === "hello");
  * ```
  */
-export function filter<T>(option: Option<T>, fn: (_: T) => boolean): Option<T> {
-  return (isSome(option) && fn(option)) ? option : undefined;
+export function zip<T, U>(
+  option: Option<T>,
+  other: Option<U>,
+): Option<[T, U]> {
+  return isSome(option) ? (isSome(other) ? [option, other] : other) : option;
 }
 
-export default { isSome, isNone, map, filter };
+/**
+ * Returns the values of `option` as a tuple of {@linkcode Option}s.
+ *
+ * @example
+ * ```ts
+ * import { assert } from "../test_deps.ts";
+ * import O from "./option.ts";
+ *
+ * const unzipped = O.unzip([2, 4]);
+ *
+ * assert(unzipped[0] === 2);
+ * assert(unzipped[1] === 4);
+ * ```
+ */
+export function unzip<T extends Some<unknown>, U extends Some<unknown>>(
+  option: Option<[T, U]>,
+): [Option<T>, Option<U>] {
+  return isSome(option) ? [option[0], option[1]] : [option, option];
+}
+
+export default { isSome, isNone, either, map, zip, unzip };
--- src/option_test.ts
+++ src/option_test.ts
@@ -1,47 +1,141 @@
-import { assert, assertFalse, assertStrictEquals } from "../test_deps.ts";
-import { default as O } from "./option.ts";
+import {
+  assert,
+  assertArrayIncludes,
+  assertFalse,
+  assertStrictEquals,
+} from "../test_deps.ts";
+import O from "./option.ts";
 
-Deno.test("is_some", async (t) => {
-  await t.step("0", () => assert(O.isSome(0)));
-  await t.step("1", () => assert(O.isSome(1)));
-  await t.step("false", () => assert(O.isSome(false)));
-  await t.step("true", () => assert(O.isSome(true)));
-  await t.step("undefined", () => assertFalse(O.isSome(undefined)));
-  await t.step("null", () => assertFalse(O.isSome(null)));
+Deno.test("isSome", async (t) => {
+  await t.step("truthy", async (t) => {
+    await t.step("true", () => assert(O.isSome(true)));
+    await t.step("Infinity", () => assert(O.isSome(Infinity)));
+    await t.step("12.3", () => assert(O.isSome(12.3)));
+    await t.step("-12.3", () => assert(O.isSome(-12.3)));
+    await t.step("123n", () => assert(O.isSome(123n)));
+    await t.step("-123n", () => assert(O.isSome(-123n)));
+    await t.step("{}", () => assert(O.isSome({})));
+  });
+
+  await t.step("falsy", async (t) => {
+    await t.step("false", () => assert(O.isSome(false)));
+    await t.step("NaN", () => assert(O.isSome(NaN)));
+    await t.step("0", () => assert(O.isSome(0)));
+    await t.step("-0", () => assert(O.isSome(-0)));
+    await t.step("0n", () => assert(O.isSome(0n)));
+    await t.step('""', () => assert(O.isSome("")));
+
+    await t.step("nullish", async (t) => {
+      await t.step("undefined", () => assertFalse(O.isSome(undefined)));
+      await t.step("null", () => assertFalse(O.isSome(null)));
+    });
+  });
 });
 
-Deno.test("is_none", async (t) => {
-  await t.step("0", () => assertFalse(O.isNone(0)));
-  await t.step("1", () => assertFalse(O.isNone(1)));
-  await t.step("false", () => assertFalse(O.isNone(false)));
-  await t.step("true", () => assertFalse(O.isNone(true)));
-  await t.step("undefined", () => assert(O.isNone(undefined)));
-  await t.step("null", () => assert(O.isNone(null)));
+Deno.test("isNone", async (t) => {
+  await t.step("truthy", async (t) => {
+    await t.step("true", () => assertFalse(O.isNone(true)));
+    await t.step("Infinity", () => assertFalse(O.isNone(Infinity)));
+    await t.step("12.3", () => assertFalse(O.isNone(12.3)));
+    await t.step("-12.3", () => assertFalse(O.isNone(-12.3)));
+    await t.step("123n", () => assertFalse(O.isNone(123n)));
+    await t.step("-123n", () => assertFalse(O.isNone(-123n)));
+    await t.step("{}", () => assertFalse(O.isNone({})));
+  });
+
+  await t.step("falsy", async (t) => {
+    await t.step("false", () => assertFalse(O.isNone(false)));
+    await t.step("NaN", () => assertFalse(O.isNone(NaN)));
+    await t.step("0", () => assertFalse(O.isNone(0)));
+    await t.step("-0", () => assertFalse(O.isNone(-0)));
+    await t.step("0n", () => assertFalse(O.isNone(0n)));
+    await t.step('""', () => assertFalse(O.isNone("")));
+
+    await t.step("nullish", async (t) => {
+      await t.step("undefined", () => assert(O.isNone(undefined)));
+      await t.step("null", () => assert(O.isNone(null)));
+    });
+  });
+});
+
+Deno.test("either", async (t) => {
+  await t.step("none", () => assertStrictEquals(O.either(undefined, 4), 4));
+  await t.step("some", () => assertStrictEquals(O.either<number>(2, 4), 2));
 });
 
 Deno.test("map", async (t) => {
-  const double = (i: number): number => i * 2;
   const toString = (i: number): string => i.toString();
 
-  await t.step("some_double", () => assertStrictEquals(O.map(8, double), 16));
-  await t.step(
-    "some_to_string",
-    () => assertStrictEquals(O.map(8, toString), "8"),
-  );
-  await t.step(
-    "none_double",
-    () => assert(O.isNone(O.map(undefined, double))),
-  );
-  await t.step(
-    "none_to_string",
-    () => assert(O.isNone(O.map(undefined, toString))),
-  );
+  await t.step("none", async (t) => {
+    await t.step(
+      "undefined",
+      () => assertStrictEquals(O.map(undefined, toString), undefined),
+    );
+    await t.step("null", () => assertStrictEquals(O.map(null, toString), null));
+  });
+  await t.step("some", () => assertStrictEquals(O.map(2, toString), "2"));
 });
 
-Deno.test("filter", async (t) => {
-  const even = (i: number): boolean => i % 2 === 0;
+Deno.test("zip", async (t) => {
+  await t.step("none none", async (t) => {
+    await t.step(
+      "undefined undefined",
+      () => assertStrictEquals(O.zip(undefined, undefined), undefined),
+    );
+    await t.step(
+      "null undefined",
+      () => assertStrictEquals(O.zip(null, undefined), null),
+    );
+    await t.step(
+      "undefined null",
+      () => assertStrictEquals(O.zip(undefined, null), undefined),
+    );
+    await t.step(
+      "null null",
+      () => assertStrictEquals(O.zip(null, null), null),
+    );
+  });
 
-  await t.step("some_even", () => assertStrictEquals(O.filter(2, even), 2));
-  await t.step("some_odd", () => assert(O.isNone(O.filter(1, even))));
-  await t.step("none", () => assert(O.isNone(O.filter(undefined, even))));
+  await t.step("none some", async (t) => {
+    await t.step(
+      "undefined",
+      () => assertStrictEquals(O.zip(undefined, 2), undefined),
+    );
+    await t.step(
+      "null",
+      () => assertStrictEquals(O.zip(null, 2), null),
+    );
+  });
+
+  await t.step("some none", async (t) => {
+    await t.step(
+      "undefined",
+      () => assertStrictEquals(O.zip(2, undefined), undefined),
+    );
+    await t.step(
+      "null",
+      () => assertStrictEquals(O.zip(2, null), null),
+    );
+  });
+
+  await t.step("some some", () => {
+    const zipped = O.zip(2, 4);
+
+    assert(O.isSome(zipped));
+    assertArrayIncludes(zipped, [2, 4]);
+  });
+});
+
+Deno.test("unzip", async (t) => {
+  await t.step("none", async (t) => {
+    await t.step(
+      "undefined",
+      () => assertArrayIncludes(O.unzip(undefined), [undefined, undefined]),
+    );
+    await t.step(
+      "null",
+      () => assertArrayIncludes(O.unzip(null), [null, null]),
+    );
+  });
+  await t.step("some", () => assertArrayIncludes(O.unzip([2, 4]), [2, 4]));
 });

08b610d6a7ea8635cdb39f5a7b7c136425d9c934
Leon Davis <leonskidev@pm.me>
2023-07-10 18:32:54 +0100

    fix: add iterator to the exports

--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
 export { default as O } from "./src/option.ts";
 export type { Option } from "./src/option.ts";
 
-// export { default as I } from "./src/iterator.ts";
-// export type { Iterator } from "./src/iterator.ts";
+export { default as I } from "./src/iterator.ts";
+export type { Iterator } from "./src/iterator.ts";

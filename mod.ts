2bf14849c765d1fbc49af90e88f9511da31e8418
Leon Davis <leonskidev@pm.me>
2023-07-10 01:26:14 +0100

    feat: add iterators
    this re-adds iterators with a much more useful design

--- mod.ts
+++ mod.ts
@@ -1,2 +1,5 @@
 export { default as O } from "./src/option.ts";
-export type { Option, Some } from "./src/option.ts";
+export type { Option } from "./src/option.ts";
+
+export { default as I } from "./src/iterator.ts";
+export type { Iterator } from "./src/iterator.ts";

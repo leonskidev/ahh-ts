ca36bcc6d480dda4de1b6eb433a5ae43f272b50d
Leon Davis <leonskidev@pm.me>
2023-07-12 04:03:52 +0100

    fix(default): better tests

--- src/default_test.ts
+++ src/default_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "../test_deps.ts";
+import { assertEquals, assertStrictEquals } from "../test_deps.ts";
 import D from "./default.ts";
 
 class Vec2 {
@@ -14,4 +14,15 @@ class Vec2 {
   }
 }
 
-Deno.test("def", () => assertEquals(D.def(Vec2), new Vec2()));
+Deno.test("def", async (t) => {
+  await t.step("default", () => assertEquals(D.def(Vec2), new Vec2()));
+  await t.step(
+    "undefined",
+    () => assertStrictEquals(D.def("undefined"), undefined),
+  );
+  await t.step("null", () => assertStrictEquals(D.def("null"), null));
+  await t.step("boolean", () => assertStrictEquals(D.def("boolean"), false));
+  await t.step("number", () => assertStrictEquals(D.def("number"), 0));
+  await t.step("bigint", () => assertStrictEquals(D.def("bigint"), 0n));
+  await t.step("string", () => assertStrictEquals(D.def("string"), ""));
+});

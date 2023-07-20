4e2c0b5b0110d68f2b0218e217e88da35d6bd482
Leon Davis <leonskidev@pm.me>
2023-07-21 00:55:41 +0100

    fix: remove default from `mod.ts`

--- mod.ts
+++ mod.ts
@@ -3,6 +3,3 @@ export type { Option } from "./src/option.ts";
 
 export { default as I } from "./src/iterator.ts";
 export type { Iterator } from "./src/iterator.ts";
-
-export { def, default as D } from "./src/default.ts";
-export type { Default } from "./src/default.ts";
